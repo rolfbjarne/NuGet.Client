8373617d52c43c1c6f2bf0108810d9c4396efcea
Birgit Hindman <birgithi@microsoft.com>
2020-10-22 20:45:33 -0700

    return NuGetFramework from GetTargetFrameworkAsync

--- src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Services/NuGetProjectManagerService.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.VisualStudio/Services/NuGetProjectManagerService.cs
@@ -14,12 +14,12 @@ using Microsoft.ServiceHub.Framework;
 using Microsoft.ServiceHub.Framework.Services;
 using Microsoft.VisualStudio.Threading;
 using NuGet.Common;
+using NuGet.Frameworks;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
 using NuGet.Packaging.Signing;
 using NuGet.ProjectManagement;
 using NuGet.ProjectManagement.Projects;
-using NuGet.ProjectModel;
 using NuGet.Protocol.Core.Types;
 using NuGet.Resolver;
 using NuGet.VisualStudio;
@@ -187,7 +187,7 @@ namespace NuGet.PackageManagement.VisualStudio
         }
 
         // Get target framework information for BuildIntegratedNuGetProjects
-        public async ValueTask<IReadOnlyCollection<TargetFrameworkInformation>> GetTargetFrameworksAsync(
+        public async ValueTask<IReadOnlyCollection<NuGetFramework>> GetTargetFrameworksAsync(
             string projectId,
             CancellationToken cancellationToken)
         {
@@ -208,12 +208,13 @@ namespace NuGet.PackageManagement.VisualStudio
                 var packageSpecs = await buildIntegratedProject.GetPackageSpecsAsync(dgcContext);
 
                 var targetFrameworks = packageSpecs
-                    .SelectMany(spec => spec.TargetFrameworks);
+                    .SelectMany(spec => spec.TargetFrameworks)
+                    .Select(f => f.FrameworkName);
 
                 return targetFrameworks.ToArray();
             }
 
-            return Array.Empty<TargetFrameworkInformation>();
+            return Array.Empty<NuGetFramework>();
         }
 
         public async ValueTask<IProjectMetadataContextInfo> GetMetadataAsync(string projectId, CancellationToken cancellationToken)
